354859b1d2f4c1583c3246d85ea68c9020a9e4fb
cook1fan <32582048@qq.com>
2017-10-20 11:25:43 +1300

    新 seed-spring

---
+++ src/main/java/com/nzh/seed/SeedSpringApplication.java
@@ -0,0 +1,12 @@
+package com.nzh.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedSpringApplication.class, args);
+	}
+}
